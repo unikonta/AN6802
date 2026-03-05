7266830894ed2864d8cd3872571915599cca657a
Cao Jiaqi <davidc20020730@gmail.com>
2026-03-05 12:18:32 +0000

    1

--- app.py
+++ app.py
@@ -7,5 +7,23 @@ app = Flask(__name__)
 def index():
     return(render_template("index.html"))
 
-if __name__ == "_main_":
+
+@app.route("/main",methods=["GET","POST"])
+def main():
+    return(render_template("main.html"))
+
+
+@app.route("/ethics",methods=["GET","POST"])
+def ethics():
+    return(render_template("ethics.html"))
+
+@app.route("/correct",methods=["GET","POST"])
+def correct():
+    return(render_template("correct.html"))
+
+@app.route("/wrong",methods=["GET","POST"])
+def wrong():
+    return(render_template("wrong.html"))
+
+if __name__ == "__main__":
     app.run()
